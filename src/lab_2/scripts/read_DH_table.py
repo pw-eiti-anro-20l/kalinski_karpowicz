19733b9b191208264c4851be6475655d650a02cb
Marcel <marcelos.kalinski@gmail.com>
2020-04-09 19:11:22 +0200

    zmiana na python2.7, zeby uzyt ros.tf.transforms

--- src/lab_2/scripts/read_DH_table.py
+++ src/lab_2/scripts/read_DH_table.py
@@ -1,20 +1,22 @@
+#! /usr/bin/python
+
 # funkcja, read_DH(path_to_DH_table), tworzy plik params.yaml w folderze config i zwraca do niego sciezke.
+from tf.transformations import translation_matrix, rotation_matrix, \
+     concatenate_matrices, euler_from_matrix, translation_from_matrix
 from os.path import join
-import itertools
 import csv
 import math
-import PyKDL as pykdl
 
-table = {'i':[], 'a':[], 'd':[], 'alfa':[], 'theta':[]}
+table = {'i':[], 'a':[], 'd':[], 'alpha':[], 'theta':[]}
 
 def dh_2_rpy(table_name, config_file):
     lines_read = 0 #nie bierzemy pod uwage 1 wiersza
 
     with open(join('config', table_name), 'r') as f:
-        reader = csv.DictReader(f, dialect=csv.unix_dialect)
+        reader = csv.DictReader(f, delimiter=' ')
         for row in reader: 
             for key in list(row):
-                table[key].append(row[key])
+                table[key].append(float(row[key]))
             lines_read+=1
 
     print(table)
@@ -25,30 +27,38 @@ def dh_2_rpy(table_name, config_file):
     pitch = []
     yaw = []
 
-    for i in range(0, lines_read):
-        rot = PyKDL.Rotation.RotX(0)
-        rot.DoRotX(float(table['alpha'][i]))
-        rot.DoRotZ(float(table['theta'][i]))
-        rpy = rot.GetRPY()
-        roll.append(rpy[0])
-        pitch.append(rpy[1])
-        yaw.append(rpy[2])
+    xes = []
+    yes = []
+    zes = []
 
-    x = []
-    y = []
-    z = []
-    for i in range(0, lines_read):
-	    x.append(float(table['a'][i]))
-	    y.append(float(table['d'][i]) * math.sin(float(table['alpha'][i])))
-	    z.append(float(table['d'][i]) * math.cos(float(table['alpha'][i])))
+    x_axis = (1, 0, 0)
+    z_axis = (0, 0, 1)
 
-    with open(join('../config', config_file), 'w+') as f:
+    for i in range(0, lines_read):
+        tz = translation_matrix((0, 0, table['d'][i]))  
+        rz = rotation_matrix(table['theta'][i], z_axis)    
+        tx = translation_matrix((table['a'][i], 0, 0))  
+        rx = rotation_matrix(table['alpha'][i], x_axis) 
+
+        dh_matrix = concatenate_matrices(tz, rz, tx, rx)
+
+        (r, p, y) = euler_from_matrix(dh_matrix)
+        (x_, y_, z_) = translation_from_matrix(dh_matrix)
+
+        roll.append(r)
+        pitch.append(p)
+        yaw.append(y)
+        xes.append(x_)
+        yes.append(y_)
+        zes.append(z_)
+
+    with open(join('config', config_file), 'w+') as f:
         for i in range(0, lines_read):
     	    f.write("roll%d: %.2f\n" % ((i+1), roll[i]))
     	    f.write("pitch%d: %.2f\n" % ((i+1), pitch[i]))
     	    f.write("yaw%d: %.2f\n" % ((i+1), yaw[i]))
-    	    f.write("x%d: %.2f\n" % ((i+1), x[i]))
-    	    f.write("y%d: %.2f\n" % ((i+1), y[i]))
-    	    f.write("z%d: %.2f\n" % ((i+1), z[i]))
+    	    f.write("x%d: %.2f\n" % ((i+1), xes[i]))
+    	    f.write("y%d: %.2f\n" % ((i+1), yes[i]))
+    	    f.write("z%d: %.2f\n" % ((i+1), zes[i]))
 
 dh_2_rpy('dh_table.csv', 'urdf_params.yaml')
